8c0b3671297b2e708cab9eb6e489454cb64cbf62
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-02-26 11:40:23 -0600

    Components made -> axiosWithAuth created -> Protected Route created -> when logged in, list of friends render to screen -> TODO: Make an add friend feature

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import FriendList from './components/FriendList';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/protected">Protected Page</Link></li>
+      </ul>
+      <Switch>
+        {/* Protected Route */}
+        <PrivateRoute exact path="/protected" component={FriendList} />
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+
+const FriendList = () => {
+  const [friends, setFriends] = useState([])
+
+    useEffect(() => {
+        axiosWithAuth().get(`http://localhost:5000/api/friends`)
+        .then( res => {
+            console.log(res.data);
+            setFriends(res.data);
+        })
+        .catch( err => {
+            console.log(err)
+        })
+    }, []);
+
+  return(
+    <div>
+      <h2>These are my friends!</h2>
+      {friends.map(friend => {
+        return (
+          <div key={friend.id} style={{width:'400px', border:'solid 1px black', display:'block', margin:'0 auto'}}>
+            <h3>{friend.name}</h3>
+            <div style={{display:'flex', justifyContent:'space-around'}}>
+              <p>{friend.age}</p>
+              <p>{friend.email}</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+
+  );
+}
+
+export default FriendList;
